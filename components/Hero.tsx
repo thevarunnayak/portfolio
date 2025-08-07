eecf8a6c0475c57b344bf597dc86cc5639f84a2d
varun.nayak <iamvarunnayak@gmail.com>
2025-08-07 23:54:57 +0530

    first commit, installed dependencies and implemented the hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { GridBackgroundDemo } from './ui/Background'
+import { TextGenerateEffect } from './ui/TextGenerate'
+import heroData from '../public/locales/en/hero.json' assert { type: 'json' }
+import MagicButton from './MagicButton'
+import { FaLocationArrow } from 'react-icons/fa6'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+        <GridBackgroundDemo />
+        <div className='flex justify-center relative my-20 z-10'>
+          <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+            <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+              {heroData.subtitle}
+            </h2>
+            <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl' words={heroData.title} />
+            <p className='text-center text-blue-100 md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>{heroData.description}</p>
+                      <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ components/ui/Background.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export function GridBackgroundDemo() {
+  return (
+    <div className="absolute top-0 left-0 flex h-screen w-full items-center justify-center bg-black-100">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:50px_40px]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#2626226f_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+        )}
+      />
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
+    </div>
+  );
+}
